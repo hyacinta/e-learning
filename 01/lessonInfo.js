756eb579d23673b93fdf82d9f9906b1f86ab9ec3
kimkyuree <kimkyuree84@gmail.com>
2023-07-18 13:51:29 +0900

    feat : 북마크 및 퀴즈 정보 입력

--- 01/lessonInfo.js
+++ 01/lessonInfo.js
@@ -3,10 +3,9 @@ const bookMarkInfo = [
   {
     id: 1,
     synkTime: "00:03",
-    title: "백다은 선생님의 Q & A 백다은 선생님의 Q & A",
+    title: "HTML Tags (Element)",
   },
-  { id: 2, synkTime: "00:06", title: "백다은 선생님의 Q & A" },
-  { id: 3, synkTime: "00:09", title: "백다은 선생님의 Q & A" },
+  { id: 2, synkTime: "00:06", title: "CSS 문법과 선언방식, 단위" },
 ];
 
 // 퀴즈 정보
@@ -26,49 +25,39 @@ const quizInfo = [
     type: "sa",
     oxQuestion: "",
     question:
-      "앱 사용 빈도순으로 보았을 때, 앱을 효율적으로 관리하기 위한 정리 순서로 <em>아닌</em> 것은?",
-    additionalType: "code",
-    additional: ["ㄱ. ㄹㅇㄴ", "ㄴ. ㅇㄴㅇㄹ", "ㄷ. ㄹㅇㄴ", "ㄹ. ㅇㅁㄴㅇ"],
-    distractor: [
-      "ㄱ, ㄴ, ㄷ, ㄹ",
-      "ㄱ, ㄴ, ㄷ, ㄹ",
-      "ㄱ, ㄴ, ㄷ, ㄹ",
-      "ㄱ, ㄴ, ㄷ, ㄹ",
-    ],
-    answer: ["4", "3"],
+      "HTML5에서는 정보 구조를 명확히 할 수 있도록 '아웃라인 알고리즘'이라는 개념이 도입되어 기존에 암묵적으로 그리던 아웃라인을 명시적이고 의미론적으로 작성할 수 있게 되었다. 다음중 HTML5에서 새롭게 도입된 태그가 아닌 것을 고르시오.",
+    additionalType: "null",
+    additional: [],
+    distractor: ["<section>", "<aside>", "<div>", "<article>"],
+    answer: ["3"],
     explanation:
-      "앱 사용 빈도순으로 보았을 때, 앱을 효율적으로 관리하기 위해 'Dock(덕) ⟶ 홈 화면 첫 페이지 활용 ⟶ 위젯 활용' 순서로 정리합니다.",
+      "<div> 태그는 기존에 존재하고 있는 태그로서 아무런 의미를 가지고 있지 않는다.",
   },
   {
     id: 2,
-    type: "ox",
-    oxQuestion: "아래의 설명이 맞으면 O, 틀리면 X를 선택하세요.",
+    type: "sa",
+    oxQuestion: "",
     question:
-      "앱 사용 빈도순으로 보았을 때, 앱을 효율적으로 관리하기 위한 정리 순서로 <em>아닌</em> 것은?",
-    additionalType: null,
+      "HTML5.2에서 정의형 목록(dl)의 자식요소로 올 수 있는 요소가 아닌 것을 고르시오.",
+    additionalType: "null",
     additional: [],
-    distractor: [],
-    answer: ["X"],
+    distractor: ["<div>", "<dt>", "<dd>", "<a>"],
+    answer: ["4"],
     explanation:
-      "앱 사용 빈도순으로 보았을 때, 앱을 효율적으로 관리하기 위해 'Dock(덕) ⟶ 홈 화면 첫 페이지 활용 ⟶ 위젯 활용' 순서로 정리합니다.",
+      "<dt>, <dd>의 경우 기존에도 <dl>의 자식요소로 올 수 있었으며, <div>의 경우에는 HTML5.2에서 <dt>, <dd>를 감싸서 사용할 수 있도록 <dl>의 자식요소로 사용할 수 있게 되었다. 단, <div>를 <dt>, <dd>를 묶기 위하여 사용할 경우 모든 <dt>, <dd>를 감싸야 하며, 일부는 감싸고, 일부는 감싸지 않는 식으로 사용할 수는 없다.",
   },
   {
     id: 3,
-    type: "ju",
+    type: "sa",
     oxQuestion: "",
     question:
-      "앱 사용 빈도순으로 보았을 때, 앱을 효율적으로 관리하기 위한 정리 순서로 <em>아닌</em> 것은?",
-    additionalType: null,
-    additional: "",
-    distractor: [
-      "ㄱ, ㄴ, ㄷ, ㄹ",
-      "ㄱ, ㄴ, ㄷ, ㄹ",
-      "ㄱ, ㄴ, ㄷ, ㄹ",
-      "ㄱ, ㄴ, ㄷ, ㄹ",
-    ],
-    answer: ["정답"],
+      "focus란 선택이 되거나 마우스 이벤트 등을 받은 상태를 의미하며, HTML의 태그들 중에는 특별한 속성을 부여하지 않아도 focus를 받을 수 있는 태그들이 존재한다. 이 중 focus를 기본 속성으로 갖는 태그를 모두 고르시오.",
+    additionalType: "null",
+    additional: [],
+    distractor: ["<a>", "<button>", "<div>", "<input>", "<span>"],
+    answer: ["1", "2", "4"],
     explanation:
-      "앱 사용 빈도순으로 보았을 때, 앱을 효율적으로 관리하기 위해 'Dock(덕) ⟶ 홈 화면 첫 페이지 활용 ⟶ 위젯 활용' 순서로 정리합니다. 앱 사용 빈도순으로 보았을 때, 앱을 효율적으로 관리하기 위해 'Dock(덕) ⟶ 홈 화면 첫 페이지 활용 ⟶ 위젯 활용' 순서로 정리합니다. 앱 사용 빈도순으로 보았을 때, 앱을 효율적으로 관리하기 위해 'Dock(덕) ⟶ 홈 화면 첫 페이지 활용 ⟶ 위젯 활용' 순서로 정리합니다.",
+      "사용자와 상호작용을 하기 위한 interactive content의 경우에 특별한 속성을 부여하지 않아도 focus를 받을 수 있다.",
   },
 ];
 
